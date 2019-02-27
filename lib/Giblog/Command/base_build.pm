601c7ea284cdcc22122649249e45e941b997529c
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-02-27 12:18:26 +0900

    add parser_giblog_syntax

--- lib/Giblog/Command/base_build.pm
+++ lib/Giblog/Command/base_build.pm
@@ -9,6 +9,7 @@ use Carp 'confess';
 use File::Basename 'dirname';
 use File::Path 'mkpath';
 use Encode 'encode', 'decode';
+use Giblog::Util;
 
 sub run {
   my $self = shift;
@@ -93,8 +94,6 @@ sub build {
   }
 }
 
-my $inline_elements_re = qr/^<(span|em|strong|abbr|acronym|dfn|q|cite|sup|sub|code|var|kbd|samp|bdo|font|big|small|b|i|s|strike|u|tt|a|label|object|applet|iframe|button|textarea|select|basefont|img|br|input|script|map)\b/;
-
 sub build_html {
   my ($self, $data) = @_;
   
@@ -176,51 +175,9 @@ sub parse_content {
   
   my $content = $data->{content};
   
-  # Normalize line break;
-  $content =~ s/\x0D\x0A|\x0D|\x0A/\n/g;
-  
   # Parse Giblog syntax
-  my @lines = split /\n/, $content;
-  my $pre_start;
-  $content = '';
-  my $bread_end;
-  for my $line (@lines) {
-    my $original_line = $line;
-    
-    # Pre end
-    if ($line =~ m|^</pre\b|) {
-      $pre_start = 0;
-    }
-    
-    # Escape >, < in pre tag
-    if ($pre_start) {
-      $line =~ s/>/&gt;/g;
-      $line =~ s/</&lt;/g;
-      $content .= "$line\n";
-    }
-    else {
-      # If start with inline tag, wrap p
-      if ($line =~ $inline_elements_re) {
-        $content .= "<p>\n  $line\n</p>\n";
-      }
-      # If start with space or tab or not inline tag, it is raw line
-      elsif ($line =~ /^[ \t\<]/) {
-        $content .= "$line\n";
-      }
-      # If line have length, wrap p
-      else {
-        if (length $line) {
-          $content .= "<p>\n  $line\n</p>\n";
-        }
-      }
-    }
-
-    # Pre start
-    if ($original_line =~ m|^<pre\b|) {
-      $pre_start = 1
-    }
-  }
-
+  Giblog::Util::parse_giblog_syntax($data);
+  
   # title
   my $path = $data->{path};
   my $path_tmp = $path;
@@ -269,10 +226,15 @@ sub parse_content {
   
   # Create description from first p tag
   unless (defined $data->{'description'}) {
-    if ($content =~ m|<\s?p\b[^>]*?>\s*(\S*?)\s*<\s?/\s?p\s?>|s) {
+    if ($content =~ m|<\s?p\b[^>]*?>(.*?)<\s?/\s?p\s?>|s) {
       my $description = $1;
       # remove tag
       $description =~ s/<.*?>//g;
+      
+      # trim space
+      $description =~ s/^\s+//;
+      $description =~ s/\s+$//;
+      
       $data->{'description'} = $description;
     }
   }
---
+++ lib/Giblog/Util.pm
@@ -0,0 +1,61 @@
+package Giblog::Util;
+
+use strict;
+use warnings;
+
+my $inline_elements_re = qr/^<(span|em|strong|abbr|acronym|dfn|q|cite|sup|sub|code|var|kbd|samp|bdo|font|big|small|b|i|s|strike|u|tt|a|label|object|applet|iframe|button|textarea|select|basefont|img|br|input|script|map)\b/;
+
+sub parse_giblog_syntax {
+  my $data = shift;
+  
+  my $content = $data->{content};
+  
+  # Normalize line break;
+  $content =~ s/\x0D\x0A|\x0D|\x0A/\n/g;
+  
+  # Parse Giblog syntax
+  my @lines = split /\n/, $content;
+  my $pre_start;
+  $content = '';
+  my $bread_end;
+  for my $line (@lines) {
+    my $original_line = $line;
+    
+    # Pre end
+    if ($line =~ m|^</pre\b|) {
+      $pre_start = 0;
+    }
+    
+    # Escape >, < in pre tag
+    if ($pre_start) {
+      $line =~ s/>/&gt;/g;
+      $line =~ s/</&lt;/g;
+      $content .= "$line\n";
+    }
+    else {
+      # If start with inline tag, wrap p
+      if ($line =~ $inline_elements_re) {
+        $content .= "<p>\n  $line\n</p>\n";
+      }
+      # If start with space or tab or not inline tag, it is raw line
+      elsif ($line =~ /^[ \t\<]/) {
+        $content .= "$line\n";
+      }
+      # If line have length, wrap p
+      else {
+        if (length $line) {
+          $content .= "<p>\n  $line\n</p>\n";
+        }
+      }
+    }
+
+    # Pre start
+    if ($original_line =~ m|^<pre\b|) {
+      $pre_start = 1
+    }
+  }
+  
+  $data->{content} = $content;
+}
+
+1;
