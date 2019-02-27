0ab9ded79141e3b599d58cd6b2fac0e18420b7c6
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-02-27 16:04:35 +0900

    add Giblog::Util::build_all

--- lib/Giblog/Util.pm
+++ lib/Giblog/Util.pm
@@ -3,6 +3,74 @@ package Giblog::Util;
 use strict;
 use warnings;
 
+sub build_all {
+  my ($giblog, $cb) = @_;
+
+  my $templates_dir = $giblog->rel_file('templates');
+  my $public_dir = $giblog->rel_file('public');
+  
+  # Get template files
+  my @template_rel_files;
+  find(
+    {
+      wanted => sub {
+        my $template_file = $File::Find::name;
+        
+        # Skip directory
+        return unless -f $template_file;
+
+        # Skip common files
+        return if $template_file =~ /^\Q$templates_dir\/common/;
+        
+        my $template_file_base = $_;
+        
+        # Skip hidden file
+        return if $template_file_base =~ /^\./;
+        
+        my $template_rel_file = $template_file;
+        $template_rel_file =~ s/^$templates_dir//;
+        $template_rel_file =~ s/^[\\\/]//;
+        $template_rel_file = "templates/$template_rel_file";
+        
+        push @template_rel_files, $template_rel_file;
+      },
+      no_chdir => 1,
+    },
+    $templates_dir
+  );
+  
+  for my $template_rel_file (@template_rel_files) {
+    my $template_file = $giblog->rel_file($template_rel_file);
+    my $content = $giblog->slurp_file($template_file);
+    
+    my $path = $template_rel_file;
+    $path =~ s|^templates||;
+    if ($path eq '/index.html') {
+      $path = '/';
+    }
+    
+    my $data = {
+      content => $content,
+      path => $path,
+    };
+
+    # Build html
+    $cb->($giblog, $data);
+    
+    my $html = $data->{content};
+    
+    # public file
+    my $public_rel_file = $template_rel_file;
+    $public_rel_file =~ s/^templates/public/;
+    my $public_file = $giblog->rel_file("$public_rel_file");
+    my $public_dir = dirname $public_file;
+    mkpath $public_dir;
+    
+    # Write to public file
+    $giblog->write_to_file($public_file, $html);
+  }
+}
+
 my $inline_elements_re = qr/^<(span|em|strong|abbr|acronym|dfn|q|cite|sup|sub|code|var|kbd|samp|bdo|font|big|small|b|i|s|strike|u|tt|a|label|object|applet|iframe|button|textarea|select|basefont|img|br|input|script|map)\b/;
 
 sub parse_giblog_syntax {
