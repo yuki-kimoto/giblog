25589f4f3d27c61f15d4b4c47d78b2088716cea4
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-06-12 08:35:29 +0900

    improve command doc

--- lib/Giblog/Command.pm
+++ lib/Giblog/Command.pm
@@ -18,9 +18,17 @@ sub run { confess 'Method "run" not implemented by subclass' }
 
 1;
 
+=encoding utf8
+
 =head1 NAME
 
 Giblog::Command - command base class
+
+=head1 DESCRIPTION
+
+L<Giblog::Command> is command base class.
+
+You can also create your command inheriting L<Giblog::Command> like L<Giblog::Command::new>, L<Giblog::Command::add> or L<Giblog::Command::build>.
 
 =head1 METHODS
 
@@ -29,6 +37,16 @@ Giblog::Command - command base class
   $command->new(%args);
 
 Create command object.
+
+Arguments:
+
+=over 2
+
+=item * api
+
+L<Giblog::API> object.
+
+=back
 
 =head2 api
 
@@ -40,4 +58,4 @@ Get L<Giblog::API> object.
 
   $command->run(@args);
 
-Run command. This method is implemented by subclass
+Run command. This method is implemented by subclass.
